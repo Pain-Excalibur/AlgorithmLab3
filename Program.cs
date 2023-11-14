7b5c335051d2fcb251b997bad659cc9572a437e5
Pain-Excalibur <ffggh.fn2@gmail.com>
2023-11-15 00:28:25 +0500

    stack 4 12/12

--- Program.cs
+++ Program.cs
@@ -13,12 +13,18 @@ class Program
         //Tasks.Start();
         //Task3.Start();
         OurStack s = new OurStack();
-
+        
         s.Push("1");
         s.Push("2");
         s.Push("3");
         s.Push("4");
-        s.TopToBot();
+        s.Push("5");
+        s.Push("6");
+        s.Push("7");
+        s.Push("8");
+        s.Push("9");
+        s.Push("10");
+        s.Swap("7","2");
         s.Print();
     }
 
