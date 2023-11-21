affbf9d610df77149a98a6c5be872a7fff522bfe
Pain-Excalibur <66920868+Pain-Excalibur@users.noreply.github.com>
2023-11-21 15:12:37 +0500

    Pain excalibur (#15)
    * final v1
    
    * final v2
    
    * 2/12 t4
    
    * stack 4 12/12
    
    * all besides 4.2
    
    * Rewrote OQ OS
    Added OLL
    Task 4 is now broken

--- OurQueue.cs
+++ OurQueue.cs
@@ -3,72 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static AlgorithmLab3.OurStack;
 
 namespace AlgorithmLab3
 {
 
-    public class OurQueue
+    public class OurQueue<T> : OurLinkedList<T>
     {
-        public QueueNode first;
-        public QueueNode last;
-
-        public void Enqueue(string data)
+        public void Enqueue(T data)
         {
-            QueueNode newElement = new QueueNode(data);
-            if (first == null) 
-            {
-                first = last = newElement;
-            }
-            else
-            {
-                last.next = newElement;
-                last = newElement;
-            }
+            this.AddToBot(data);
         }
-        public string Dequeue()
+        public T Dequeue()
         {
-            string peek = null;
-            if (first != null)
-            {
-                peek = first.data;
-                first = first.next;
-            }
-            return peek;
+            return this.RemoveFromTop().Data;
         }
-        public string Peek()
+        public T Peek()
         {
-            if (first == null)
-                return null;
-            return first.data;
-        }
-
-        public bool IsEmpty()
-        {
-            return first == null;
-        }
-        public void Print()
-        {
-            QueueNode current = first;
-
-            while (current != null)
-            {
-                Console.Write(current.data + " ");
-                current = current.next;
-            }
+            return this.GetTop().Data;
         }
     }
-
-    public class QueueNode
-    {
-        public string data;
-        public QueueNode next;
-
-        public QueueNode(string data) 
-        {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
 }
